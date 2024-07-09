da2d0334f79826f8cb896c031078abbc52375077
tominaga <tominaga@aibase.co.jp>
2024-07-09 17:09:36 +0900

    Add PostgreSQL support and update model definitions
    Added PostgreSQL connectivity in settings and introduced a build script for Django project. The model definitions were updated with changes to primary key handling and a progress bar was added for fixtures generation. Additionally, the Django's static file storage is set to use the WhiteNoise storage backend for improved performance with compressed files.

--- server/wsgi.py
+++ server/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+import environ
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+env = environ.Env(DEBUG=(bool, False))
+env.read_env(os.path.join(os.getcwd(), '.env'))
+if env('DEBUG'):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.local')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.production')
 
 application = get_wsgi_application()
